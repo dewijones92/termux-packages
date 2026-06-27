e7f120fa4ffa1a1f28a82147dd557e689cfe6320
Dewi Jones <dewijones92@gmail.com>
2026-06-27 22:46:59 +0100

    aria2: drop c-ares for API 23 (prebuilt dep needs getifaddrs@LIBC_N)

--- packages/aria2/build.sh
+++ packages/aria2/build.sh
@@ -3,13 +3,15 @@ TERMUX_PKG_DESCRIPTION="Download utility supporting HTTP/HTTPS, FTP, BitTorrent 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.37.0"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL="https://github.com/aria2/aria2/releases/download/release-${TERMUX_PKG_VERSION}/aria2-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag" # As of 2022-05-11T00:08:00 no github releases are available.
-TERMUX_PKG_DEPENDS="libc++, c-ares, openssl, libxml2, zlib"
+# c-ares dropped for API 23: it's a prebuilt-for-API-24 lib that references getifaddrs@LIBC_N
+# (unavailable on API 23). aria2 falls back to synchronous name resolution without it.
+TERMUX_PKG_DEPENDS="libc++, openssl, libxml2, zlib"
 # sqlite3 is only used for loading cookies from firefox or chrome:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-openssl
@@ -17,6 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-libuv
 --without-sqlite3
 --without-libssh2
+--without-libcares
 ac_cv_func_basename=yes
 ac_cv_func_getaddrinfo=yes
 ac_cv_func_gettimeofday=yes
